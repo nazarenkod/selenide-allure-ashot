5827e8d43b27421be0a2342abb51a900f4586f62
Dmitriy <nazarenkod1990@gmail.com>
2018-01-12 18:41:47 +0200

    Update TestBase.java

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -4,10 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import utils.PropertyLoader;
-import utils.ScreenShotListener;
-
 import java.io.IOException;
-
 import static com.codeborne.selenide.Selenide.open;
 import static utils.ScreenShotListener.createFolders;
 
@@ -15,17 +12,17 @@ import static utils.ScreenShotListener.createFolders;
 
 public class TestBase {
 
-    public static String browserSize = PropertyLoader.getInstance().getProperty("browserSize");
+    public static String browserSize;
 
 
     @BeforeSuite
     public void init(){
-
+        browserSize = PropertyLoader.getInstance().getProperty("browserSize");
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://www.google.com.ua";
         Configuration.browserSize = browserSize;
         try {
-            createFolders("src/test/screenshots");
+            createFolders();
         } catch (IOException e) {
             e.printStackTrace();
         }
